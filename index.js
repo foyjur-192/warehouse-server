4d583fef5d920b003b7e31d3789cb2ca1410ecc9
foyjur-192 <97134368+foyjur-192@users.noreply.github.com>
2022-05-09 23:42:37 +0600

    newupdateagain

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ res.send(result);
 
 
 
-  //Delete
+  //Delete the data
   app.delete('/computerInventory/:id', async(req, res) => {
     const id = req.params.id;
     const query = {_id: ObjectId(id)};
